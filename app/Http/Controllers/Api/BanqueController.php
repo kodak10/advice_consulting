4cb2af25b3ef72ac53c97526f2be1f9da41e8e60
Kodak10 <atchinaymard10@gmail.com>
2025-09-30 10:16:46 +0000

    fin de banque

--- app/Http/Controllers/Api/BanqueController.php
+++ app/Http/Controllers/Api/BanqueController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Banque;
+use Illuminate\Validation\Rule;
 
 class BanqueController extends Controller
 {
@@ -12,5 +13,47 @@ class BanqueController extends Controller
     {
         $banques = Banque::all();
         return response()->json($banques);
+    }
+
+     public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'num_compte' => 'string|regex:/^[A-Z0-9 ]+$/|unique:banques,num_compte',
+        ]);
+
+        $banque = Banque::create([
+            'name' => $validated['name'],
+            'num_compte' => $validated['num_compte'],
+        ]);
+
+        return response()->json(['message' => 'Banque créé avec succès']);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $banque = Banque::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'num_compte' => 'required|string|regex:/^[A-Z0-9 ]+$/|unique:banques,num_compte,' . $banque->id,
+        ]);
+
+        $banque->update([
+            'name' => $validated['name'],
+            'num_compte' => $validated['num_compte'],
+        ]);
+
+
+        return response()->json(['message' => 'Banque mis à jour avec succès']);
+    }
+
+    public function destroy(Banque $banque)
+    {
+        $banque->delete();
+
+        return response()->json([
+            'message' => 'Banque supprimée avec succès'
+        ]);
     }
 }
